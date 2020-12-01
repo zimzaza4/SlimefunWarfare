207118709cad6ba9eb87fe474c0eacc75e8d4622
zimzaza4 <53421735+zimzaza4@users.noreply.github.com>
2020-12-01 18:27:14 +0800

    Update Items.java

--- src/main/java/io/github/seggan/slimefunwarfare/lists/Items.java
+++ src/main/java/io/github/seggan/slimefunwarfare/lists/Items.java
@@ -14,59 +14,55 @@ import org.bukkit.NamespacedKey;
 public final class Items {
     public static final Category sfwarfareCategory = new Category(
         new NamespacedKey(SlimefunWarfare.getInstance(), "slimefunwarfare"),
-        new CustomItem(Material.DIAMOND_SWORD, "Slimefun Warfare")
+        new CustomItem(Material.DIAMOND_SWORD, "粘液战争")
     );
     public static final Category sfwarfareGunsCategory = new Category(
         new NamespacedKey(SlimefunWarfare.getInstance(), "slimefunwarfare_guns"),
-        new CustomItem(Material.CROSSBOW, "Slimefun Warfare - Guns")
+        new CustomItem(Material.CROSSBOW, "粘液战争 - 枪支")
     );
     public static final Category sfwarfareExplosivesCategory = new Category(
         new NamespacedKey(SlimefunWarfare.getInstance(), "slimefunwarfare_explosives"),
-        new CustomItem(Material.TNT, "Slimefun Warfare - Explosives")
+        new CustomItem(Material.TNT, "粘液战争 - 炸药")
     );
 
     public static final SlimefunItemStack SLIMESTEEL = new SlimefunItemStack(
         "SLIMESTEEL_INGOT",
         Material.IRON_INGOT,
-        "&aSlimesteel Ingot",
+        "&a滑钢锭",
         "",
-        "Hard but elastic, this alloy",
-        "is perfect for all sorts",
-        "of applications"
+        "坚硬但富有弹性，这种合金非常适合各种应用"
     );
 
     public static final SlimefunItemStack REINFORCED_SLIMESTEEL = new SlimefunItemStack(
         "REINFORCED_SLIMESTEEL_INGOT",
         Material.IRON_INGOT,
-        "&aReinforced Slimesteel Ingot",
+        "&a强化软钢锭",
         "",
-        "Hard but elastic, this alloy",
-        "is perfect for all sorts",
-        "of applications"
+        "坚硬但富有弹性，这种合金非常适合各种应用"
     );
 
     public static final SlimefunItemStack SCOPE = new SlimefunItemStack(
         "SCOPE",
         Material.STICK,
-        "&aScope"
+        "&a范围"
     );
 
     public static final SlimefunItemStack BARREL = new SlimefunItemStack(
         "BARREL",
         Material.STICK,
-        "&7Barrel"
+        "&7枪管"
     );
 
     public static final SlimefunItemStack ADVANCED_BARREL = new SlimefunItemStack(
         "ADVANCED_BARREL",
         Material.STICK,
-        "&7Advanced Barrel"
+        "&7高级枪管"
     );
 
     public static final SlimefunItemStack BULLET_PRESS = new SlimefunItemStack(
         "BULLET_PRESS",
         Material.SMOKER,
-        "&7Bullet Press",
+        "&7弹药制造机",
         LoreBuilder.powerPerSecond(16),
         LoreBuilder.powerBuffer(32),
         LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE)
@@ -75,7 +71,7 @@ public final class Items {
     public static final SlimefunItemStack AIR_LIQUEFIER = new SlimefunItemStack(
         "AIR_LIQUEFIER",
         Material.BEACON,
-        "&bAir Liquefier",
+        "&b空气液化器",
         LoreBuilder.powerPerSecond(64),
         LoreBuilder.powerBuffer(128),
         LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE)
@@ -84,7 +80,7 @@ public final class Items {
     public static final SlimefunItemStack EXPLOSIVE_SYNTHESIZER = new SlimefunItemStack(
         "EXPLOSIVE_SYNTHESIZER",
         Material.TNT,
-        "&4Explosive Synthesizer",
+        "&4爆炸合成器",
         LoreBuilder.powerPerSecond(64),
         LoreBuilder.powerBuffer(128),
         LoreBuilder.machine(MachineTier.ADVANCED, MachineType.MACHINE)
@@ -93,72 +89,72 @@ public final class Items {
     public static final SlimefunItemStack IRON_BULLET = new SlimefunItemStack(
         "IRON_BULLET",
         Material.IRON_NUGGET,
-        "&7Iron Bullet",
-        "&7x0.75 damage"
+        "&7铁子弹",
+        "&7x0.75伤害"
     );
 
     public static final SlimefunItemStack LEAD_BULLET = new SlimefunItemStack(
         "LEAD_BULLET",
         Material.IRON_NUGGET,
-        "&7Lead Bullet",
-        "&7x1 damage"
+        "&7铅弹",
+        "&7x1伤害"
     );
 
     public static final SlimefunItemStack DU_BULLET = new SlimefunItemStack(
         "DU_BULLET",
         Material.IRON_NUGGET,
-        "&aDU Bullet",
-        "&7Sets hit entities on fire",
-        "&7x1.5 damage"
+        "&aDU弹",
+        "&7让命中的实体着火",
+        "&7x1.5伤害"
     );
 
     public static final SlimefunItemStack GOLD_BULLET = new SlimefunItemStack(
         "GOLD_BULLET",
         Material.GOLD_NUGGET,
-        "&6Gold Bullet",
-        "&7x2 damage"
+        "&6金子弹",
+        "&7x2伤害"
     );
 
     public static final SlimefunItemStack TRINITROBULLETENE = new SlimefunItemStack(
         "TRINITROBULLETENE_BULLET",
         Material.GOLD_NUGGET,
-        "&6Trinitrobulletene",
-        "&7Sets hit entities on fire",
-        "&7x2.75 damage"
+        "&6三硝基丁烯弹",
+        "&7让命中的实体着火",
+        "&7x2.75伤害"
     );
 
     public static final SlimefunItemStack GUN_CASE = new SlimefunItemStack(
         "GUN_CASE",
         Material.CROSSBOW,
-        "&7Gun Case",
-        "&7The base of all guns"
+        "&7枪盒",
+        "&7所有枪的基础"
     );
 
     public static final SlimefunItemStack PYRO_POWDER = new SlimefunItemStack(
         "PYRO_POWDER",
         Material.REDSTONE,
-        "&4Pyro Powder",
-        "&7A very explosive substance indeed"
+        "&4火热粉",
+        "&7确实是一种极易爆炸的物质"
     );
 
     public static final SlimefunItemStack LIQUID_AIR = new SlimefunItemStack(
         "LIQUID_AIR",
         HeadTexture.TIN_CAN,
-        "&fLiquid Air",
-        "&7Don't touch!"
+        "&f液态空气",
+        "&7不要碰!"
     );
 
     public static final SlimefunItemStack LIQUID_NITROGEN = new SlimefunItemStack(
         "LIQUID_NITROGEN",
         HeadTexture.TIN_CAN,
-        "&fLiquid Nitrogen",
-        "&7Ice cream anyone?"
+        "&f液氮",
+        "&7冰冻任何人?"
     );
 
     public static final SlimefunItemStack PURIFIED_LIQUID_NITROGEN = new SlimefunItemStack(
         "PURIFIED_LIQUID_NITROGEN",
         HeadTexture.TIN_CAN,
-        "&fPurified Liquid Nitrogen",
-        "&7As pure as can be"
+        "&f纯化液氮",
+        "&7尽可能纯净"
     );
 }
