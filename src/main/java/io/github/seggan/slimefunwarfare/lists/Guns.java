72dd4c5fecf874fa66553847379e71750967b6c1
zimzaza4 <53421735+zimzaza4@users.noreply.github.com>
2020-12-01 18:24:22 +0800

    Update Guns.java

--- src/main/java/io/github/seggan/slimefunwarfare/lists/Guns.java
+++ src/main/java/io/github/seggan/slimefunwarfare/lists/Guns.java
@@ -8,96 +8,92 @@ public final class Guns {
     public static final SlimefunItemStack PISTOL = new SlimefunItemStack(
         "GUN_PISTOL",
         Material.CROSSBOW,
-        "&7Pistol",
-        "&7A short range gun",
-        "&7that reloads quickly.",
-        "&7Useful for close combat.",
-        "&cRange: 7",
-        "&cDamage: 2 hearts",
-        "&cCooldown: 0.75 seconds"
+        "&7手枪",
+        "&7短程枪",
+        "&7装弹很快",
+        "&7很适合近战",
+        "&c范围: 7",
+        "&c伤害: 2心",
+        "&c冷却: 0.75s"
     );
 
     public static final SlimefunItemStack REVOLVER = new SlimefunItemStack(
         "GUN_REVOLVER",
         Material.CROSSBOW,
-        "&7Revolver",
-        "&7A short range gun",
-        "&7that reloads quickly.",
-        "&7Useful for close combat.",
-        "&cRange: 10",
-        "&cDamage: 3 hearts",
-        "&cCooldown: 0.5 seconds"
+        "&7左轮手枪",
+        "&7短程枪",
+        "&7装弹很快",
+        "&7很适合近战",
+        "&c范围: 10",
+        "&c伤害: 3心",
+        "&c冷却: 0.5s"
     );
 
     public static final SlimefunItemStack MACHINE_GUN = new SlimefunItemStack(
         "GUN_MACHINE_GUN",
         Material.CROSSBOW,
-        "&7Machine Gun",
-        "&7Pew pew pew",
-        "&cRange: 30",
-        "&cMinimum Range: 5",
-        "&cDamage: 3 hearts",
-        "&cCooldown: 0.15 seconds"
+        "&7机枪",
+        "&7砰砰砰!",
+        "&c范围: 30",
+        "&c最小范围: 5",
+        "&c伤害: 3心",
+        "&c冷却: 0.15s"
     );
 
     public static final SlimefunItemStack MINIGUN = new SlimefunItemStack(
         "GUN_MINIGUN",
         Material.CROSSBOW,
-        "&7Minigun",
-        "&7The ultimate device",
-        "&7to pepper your friends with.",
-        "&cRange: 40",
-        "&cMinimum Range: 5",
-        "&cDamage: 4 hearts",
-        "&cCooldown: none"
+        "&7迷你机枪",
+        "&7与您的朋友相处的",
+        "&7终极装备",
+        "&c范围: 40",
+        "&c最小范围: 5",
+        "&c伤害: 4心",
+        "&c冷却: 无"
     );
 
     public static final SlimefunItemStack RIFLE = new SlimefunItemStack(
         "GUN_RIFLE",
         Material.CROSSBOW,
-        "&7Rifle",
-        "&7A standard rifle.",
-        "&cRange: 40",
-        "&cMinimum Range: 5",
-        "&cDamage: 4 hearts",
-        "&cCooldown: 0.75 seconds"
+        "&7步枪",
+        "&7标准步枪",
+        "&c范围: 40",
+        "&c最小范围: 5",
+        "&c伤害: 4心",
+        "&c冷却: 0.75s"
     );
 
     public static final SlimefunItemStack SHOTGUN = new SlimefunItemStack(
         "GUN_SHOTGUN",
         Material.CROSSBOW,
-        "&7Shotgun",
-        "&7Less range than a",
-        "&7rifle, but more damage.",
-        "&cRange: 25",
-        "&cMinimum Range: 5",
-        "&cDamage: 6.5 hearts",
-        "&cCooldown: 1.25 seconds"
+        "&7霰弹枪",
+        "&7攻击范围比步枪小",
+        "&7但伤害高",
+        "&c范围: 25",
+        "&c最小范围: 5",
+        "&c伤害: 6.5心",
+        "&c冷却: 1.25s"
     );
 
     public static final SlimefunItemStack ASSAULT_RIFLE = new SlimefunItemStack(
         "GUN_ASSAULT_RIFLE",
         Material.CROSSBOW,
-        "&7Assault Rifle",
-        "&7A derivative of",
-        "&7the rifle, the assault",
-        "&7rifle is the standard",
-        "&7military weapon.",
-        "&cRange: 50",
-        "&cMinimum Range: 3",
-        "&cDamage: 6.5 hearts",
-        "&cCooldown: 0.3 seconds"
+        "&7突击步枪",
+        "&7突击步枪是步枪的衍生品,是标准的军事武器",
+        "&c范围: 50",
+        "&c最小范围: 3",
+        "&c伤害: 6.5心",
+        "&c冷却: 0.3s"
     );
 
     public static final SlimefunItemStack SNIPER = new SlimefunItemStack(
         "GUN_SNIPER",
         Material.CROSSBOW,
-        "&7Sniper Rifle",
-        "&7The ultimate long-range",
-        "&7gun, the sniper is very powerful.",
-        "&cRange: 130",
-        "&cMinimum Range: 50",
-        "&cDamage: 11 hearts",
-        "&cCooldown: 8 seconds"
+        "&7狙击枪",
+        "&7终极远程枪 伤害较高",
+        "&c范围: 130",
+        "&c最小范围: 50",
+        "&c伤害: 11心",
+        "&c冷却: 8s"
     );
 }
