621e579e22d1ee287957f882cfd75f73e4f26cd6
zimzaza4 <53421735+zimzaza4@users.noreply.github.com>
2021-02-20 12:00:35 +0800

    Update Categories.java

--- src/main/java/io/github/seggan/slimefunwarfare/lists/Categories.java
+++ src/main/java/io/github/seggan/slimefunwarfare/lists/Categories.java
@@ -12,26 +12,26 @@ public final class Categories {
 
     public static final Category GENERAL = new Category(
         new NamespacedKey(SlimefunWarfare.getInstance(), "slimefunwarfare"),
-        new CustomItem(Material.DIAMOND_SWORD, "Slimefun Warfare")
+        new CustomItem(Material.DIAMOND_SWORD, "粘液战争")
     );
 
     public static final Category GUNS = new Category(
         new NamespacedKey(SlimefunWarfare.getInstance(), "slimefunwarfare_guns"),
-        new CustomItem(Material.CROSSBOW, "Slimefun Warfare - Guns")
+        new CustomItem(Material.CROSSBOW, "粘液战争 - 枪支")
     );
 
     public static final Category MELEE = new Category(
         new NamespacedKey(SlimefunWarfare.getInstance(), "slimefunwarfare_melee"),
-        new CustomItem(Material.IRON_AXE, "Slimefun Warfare - Melee Weapons")
+        new CustomItem(Material.IRON_AXE, "粘液战争 - 近战武器")
     );
 
     public static final Category EXPLOSIVES = new Category(
         new NamespacedKey(SlimefunWarfare.getInstance(), "slimefunwarfare_explosives"),
-        new CustomItem(Material.TNT, "Slimefun Warfare - Explosives")
+        new CustomItem(Material.TNT, "粘液战争 - 爆炸性武器")
     );
 
     public static final Category RESOURCES = new Category(
         new NamespacedKey(SlimefunWarfare.getInstance(), "slimefunwarfare_resources"),
-        new CustomItem(Material.IRON_ORE, "Slimefun Warfare - Resources")
+        new CustomItem(Material.IRON_ORE, "粘液战争 - 资源")
     );
 }
